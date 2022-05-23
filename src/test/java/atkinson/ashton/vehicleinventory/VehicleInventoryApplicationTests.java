3d321d67e1a9f613f9a604a527aff705ce055899
Tigressive <rascalcrafters@gmail.com>
2022-05-22 18:01:41 -0600

    Initial commit

---
+++ src/test/java/atkinson/ashton/vehicleinventory/VehicleInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package atkinson.ashton.vehicleinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
